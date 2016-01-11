848d2e8eaadb66248deaa63e96a934498214fc09
Johan Lundgren <johan.lundgren97@me.com>
2016-01-11 20:24:44 +0100

    Euler14

--- Euler14/main.m
+++ Euler14/main.m
@@ -10,8 +10,24 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int counter = 0;
+        int storst = 0;
+        for (int i = 2; i < 1000000; i++) {
+            while (i != 1) {
+                if (i % 2 == 0) {
+                    i = i / 2;
+                    counter++;
+                } else {
+                    i = 3 * i + 1;
+                    counter++;
+                }
+            }
+            if (counter > storst) {
+                storst = counter;
+            }
+            counter = 0;
+        }
+        NSLog(@"Den längsta kjedjan som kan fås fram är: %i", storst);
     }
     return 0;
 }
